94797f6c97d8386e65331b6922b545db2b708ecb
shri-suriya <shrisuriya23@gmail.com>
2024-08-08 20:56:58 +0530

    general files created

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import First from "./components/First";
+function App() {
+  return (
+    <div className="App">
+      
+        <p>
+          hii hello
+        </p>
+    <First />
+    </div>
+  );
+}
+
+export default App;
+
+
